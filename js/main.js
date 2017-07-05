cae8a2e685a3ff7e314f344a1d698cc84f048470
kathmarioly <katerinemarioly@gmail.com>
2017-07-05 12:25:00 -0400

    Cambios en el js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,9 @@
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
   });
+
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }

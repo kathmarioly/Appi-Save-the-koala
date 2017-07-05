fbebdd273c8face235241e87fe84f47a8f06a531
kathmarioly <katerinemarioly@gmail.com>
2017-07-05 12:52:10 -0400

    change button

--- js/main.js
+++ js/main.js
@@ -8,15 +8,13 @@
     FB.AppEvents.logPageView();   
   };
 
-  (function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
-
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.9&appId=227254561129192";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
@@ -27,3 +25,5 @@
       statusChangeCallback(response);
     });
   }
+
+
